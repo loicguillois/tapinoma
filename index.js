a394209fdce3445949e602733ddee1934667877a
Loïc Guillois <contact@loicguillois.fr>
2016-11-07 22:23:55 +0100

    :open and claim USB adapter

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-var usb = require('usb');
+"use strict";
 
-var devices = usb.getDeviceList();
+var usb = require('usb');
 
 const GARMIN_ID = 0x0fcf;
 const GARMIN_STICK_2 = 0x1008;
 const GARMIN_STICK_3 = 0x1009;
 
-for(var idx in devices) {
-    if(devices[idx].deviceDescriptor.idVendor === GARMIN_ID) {
-        if(devices[idx].deviceDescriptor.idProduct === GARMIN_STICK_2 || devices[idx].deviceDescriptor.idProduct === GARMIN_STICK_3) {
-            console.log("USB ANT+ ADAPTER FOUND !")
-        }
+usb.setDebugLevel(4);
+
+var device = usb.findByIds(GARMIN_ID, GARMIN_STICK_3);
+if(device === undefined) {
+    device = usb.findByIds(GARMIN_ID, GARMIN_STICK_2);
+}
+
+if(device != undefined) {
+    console.log("USB ANT+ ADAPTER FOUND !");
+    device.open();
+    let iface = device.interfaces[0];
+
+    if(iface.isKernelDriverActive()) {
+        iface.detachKernelDriver();
     }
+
+    iface.claim();
+    let InEndpoint = iface.endpoints[0];
+
+    InEndpoint.on('data', (d) => {
+        console.log("data=" + d);
+    })
+
+    InEndpoint.on('error', (e) => {
+        console.log("error=" + e);
+    })
+
+    InEndpoint.on('end', () => {
+        console.log("STOP RECIEVING");
+    })
+
+    InEndpoint.startPoll();
+} else {
+    console.log("USB ANT+ ADAPTER NOT FOUND !");
 }
