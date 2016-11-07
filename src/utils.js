9c1ef519b04e045f0737847d9e149909028d01a9
Loïc Guillois <contact@loicguillois.fr>
2016-11-07 22:50:15 +0100

    adding some constants + utils function

---
+++ src/utils.js
@@ -0,0 +1,46 @@
+require('./constants');
+
+var buildMessage = function(payload = [], msgID = 0x00) {
+    let m = []
+    m.push(MESSAGE_TX_SYNC);
+    m.push(payload.length);
+    m.push(msgID);
+    for(idex in payload) {
+      m.push(payload[idx]);
+    }
+    m.push(getChecksum(m));
+
+    return new Buffer(m);
+}
+  
+var intToLEHexArray = function(int, numBytes) {
+    if(typeof(numBytes) === "undefined" || numBytes === null) {
+        numBytes = 1;
+    }
+    let a = [];
+    let b = new Buffer(decimalToHex(int, numBytes * 2), 'hex');
+    let i = b.length - 1;
+    while(i >= 0) {
+      a.push(b[i]);
+      i--;
+    }
+    return a;
+}
+    
+var decimalToHex = function(d, numDigits) {
+    let hex = Number(d).toString(16);
+    if(typeof(numDigits) === "undefined" || numDigits === null) {
+        numDigits = 2;
+    }
+    while(hex.length < numDigits) {
+        hex = "0" + hex;
+    }
+    return hex;
+}
+
+var getChecksum = function(message) {
+    for(idx in message) {
+      checksum = (checksum ^ message[idx]) % 0xFF;
+    }
+    return checksum;
+}
